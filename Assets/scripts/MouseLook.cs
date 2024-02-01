7d94a5d94eb545310d08c4717a95f74deadf92b4
Francef <56524002+Francef@users.noreply.github.com>
2024-02-01 09:00:34 -0800

    removed unneeded scripts, added scripts to address changing view with mouse and player movement

--- Assets/scripts/MouseLook.cs
+++ Assets/scripts/MouseLook.cs
@@ -15,6 +15,12 @@ public class MouseLook : MonoBehaviour
     // public class-scope variable so it shows up in Inspector
     public RotationAxes axes = RotationAxes.MouseXAndY;
     public float sensitivityHoriz = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minVert = -45.0f;
+    public float maxVert = 45.0f;
+
+    private float rotationX = 0.0f;
 
     // Update is called once per frame
     void Update()
@@ -27,9 +33,21 @@ public class MouseLook : MonoBehaviour
         } else if (axes == RotationAxes.MouseY)
         {
             // vertical rotation here
+            rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            rotationX = Mathf.Clamp(rotationX, minVert, maxVert);
+
+            transform.localEulerAngles = new Vector3(rotationX, 0, 0);
         } else
         {
             // both horizontal and vertical rotation here
+            rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            rotationX = Mathf.Clamp(rotationX, minVert, maxVert);
+
+            // calculate change in horizontal mouse movement
+            float deltaHoriz = Input.GetAxis("Mouse X") * sensitivityHoriz;
+            float rotationY = transform.localEulerAngles.y + deltaHoriz;
+
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
         }
     }
 }
