9041b33e9ed915a1ca07ec3f2b08ed1774085ade
Francef <France_fortin1@hotmail.com>
2024-02-08 14:25:59 -0800

    AI moves and logic for firing back is in place but not quite working yet

---
+++ Assets/scripts/Laser.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float speed = 6f;
+    [SerializeField] private Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        float toNewtons = 100;
+
+        Vector3 movement = transform.forward * Time.deltaTime * speed * toNewtons;
+
+        rb.velocity = movement;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        if (player != null)
+        {
+            player.Hit();
+        }
+        Destroy(this.gameObject);
+    }
+
+}
