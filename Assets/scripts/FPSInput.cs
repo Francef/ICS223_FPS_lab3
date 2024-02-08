f4be2b1f65ea4a68268be5276b20db67ebaefc61
Francef <France_fortin1@hotmail.com>
2024-02-08 09:31:39 -0800

    Added iTween, configured enemy to fall over and disappear when shot

--- Assets/scripts/FPSInput.cs
+++ Assets/scripts/FPSInput.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class FPSInput : MonoBehaviour
 {
+    private float gravity = -9.8f;
     private float speed = 9.0f;
-    private float horizInput;
-    private float vertInput;
+    [SerializeField]
+    private CharacterController charController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,24 @@ public class FPSInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizInput = Input.GetAxis("Horizontal");
-        vertInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizInput, 0, vertInput) * Time.deltaTime * speed;
-        transform.Translate(movement);
+        float horizInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizInput, 0, vertInput);
+
+        // clamp magnitude to limit diagonal movement
+        movement = Vector3.ClampMagnitude(movement, 1.09f);
+
+        // take speed into account
+        movement *= speed;
+
+        movement.y = gravity;
+
+        // make movement processor independent
+        movement *= Time.deltaTime;
+
+        // Convert local to global coordinates
+        movement = transform.TransformDirection(movement);
+
+        charController.Move(movement);
     }
 }
