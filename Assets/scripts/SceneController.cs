931cf13fad0d421de0163d7f884755315ffb341e
Francef <France_fortin1@hotmail.com>
2024-02-15 08:57:06 -0800

    feat: multiple enemies will now spawn End of lab 5

--- Assets/scripts/SceneController.cs
+++ Assets/scripts/SceneController.cs
@@ -6,18 +6,28 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField]
     private GameObject enemyPrefab;
-    private GameObject enemy;
+    private GameObject[] enemies;
     private Vector3 spawnPoint = new Vector3 (0, 0, 5);
+    private int numEnemies = 5;
+
+    private void Start()
+    {
+        enemies = new GameObject[numEnemies];
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (enemy == null)
+        // spawn multiple enemy instances
+        for (int i = 0; i < enemies.Length; i++)
         {
-            enemy = Instantiate (enemyPrefab) as GameObject;
-            enemy.transform.position = spawnPoint;
-            float angle = Random.Range(0, 360);
-            enemy.transform.Rotate(0, angle, 0);
+            if (enemies[i] == null)
+            {
+                enemies[i] = Instantiate(enemyPrefab) as GameObject;
+                enemies[i].transform.position = spawnPoint;
+                float angle = Random.Range(0, 360);
+                enemies[i].transform.Rotate(0, angle, 0);
+            }
         }
     }
 }
