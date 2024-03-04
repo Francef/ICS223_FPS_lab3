78f3f2b2228b4050c0759804b23ec4fa5426d104
Francef <France_fortin1@hotmail.com>
2024-03-03 20:22:28 -0800

    Added iguanas spawning into scene to have some scaly friends. End of lab 7

--- Assets/scripts/SceneController.cs
+++ Assets/scripts/SceneController.cs
@@ -9,10 +9,29 @@ public class SceneController : MonoBehaviour
     private GameObject[] enemies;
     private Vector3 spawnPoint = new Vector3 (0, 0, 5);
     private int numEnemies = 5;
+    [SerializeField]
+    private GameObject iguanaPrefab;
+    private GameObject[] iguanas;
+    private int numIguanas = 10;
+    [SerializeField]
+    private Transform iguanaSpawnPt;
 
     private void Start()
     {
         enemies = new GameObject[numEnemies];
+        iguanas = new GameObject[numIguanas];
+
+        // spawn multiple iguana instances
+        for (int i = 0; i < iguanas.Length; i++)
+        {
+            if (iguanas[i] == null)
+            {
+                iguanas[i] = Instantiate(iguanaPrefab) as GameObject;
+                iguanas[i].transform.position = iguanaSpawnPt.position;
+                float angle = Random.Range(0, 360);
+                iguanas[i].transform.Rotate(0, angle, 0);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -29,5 +48,6 @@ public class SceneController : MonoBehaviour
                 enemies[i].transform.Rotate(0, angle, 0);
             }
         }
+        
     }
 }
